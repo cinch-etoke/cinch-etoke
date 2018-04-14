941bf500cfaa2e78e4f85e2b7e2935d5cade1f3d
Rob Hesketh <rhesketh.uk@gmail.com>
2018-04-14 02:41:16 +0100

    Removed old tests

--- spec/cinch/plugins/etoke/session_registry_spec.rb
+++ spec/cinch/plugins/etoke/session_registry_spec.rb
@@ -3,20 +3,6 @@ require "cinch/plugins/etoke_framework/session"
 require "cinch/plugins/etoke_framework/session_registry"
 
 RSpec.describe Cinch::Plugins::EtokeFramework::SessionRegistry do
-  describe "#create" do
-    it 'raises an error if a channel already has a session' do
-      channel = instance_double(Cinch::Channel, name: '#test', send: nil)
-      subject.create(channel: channel, starter: 'Rob')
-      expect {
-        subject.create(channel: channel, starter: 'Rob')
-      }.to raise_error Cinch::Plugins::EtokeFramework::SessionRegistry::SessionExistsForChannelError
-    end
-
-    it 'creates a new session if the existing session is finished' do
-
-    end
-  end
-
   describe "#find(channel_name)" do
     it 'finds a session for the channel name' do
       session = instance_double(Cinch::Plugins::EtokeFramework::Session)
@@ -27,10 +13,8 @@ RSpec.describe Cinch::Plugins::EtokeFramework::SessionRegistry do
       expect(subject.find('test')).to eq session
     end
 
-    it 'raises an error if the session does not exist' do
-      expect {
-        subject.find('test')
-      }.to raise_error Cinch::Plugins::EtokeFramework::SessionRegistry::SessionNotFoundError
+    it 'returns nil if the session does not exist' do
+      expect(subject.find('test')).to eq nil
     end
   end
 end
